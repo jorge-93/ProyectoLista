965a0946da7292e71603d6708d66098ecb2be04b
iCd93 <ncd793@gmail.com>
2015-04-20 18:13:15 +0200

    subida captura de sonarqube

--- proyectoDoubleLinkedList/src/main/java/dll/ListaDoblementeEnlazada.java
+++ proyectoDoubleLinkedList/src/main/java/dll/ListaDoblementeEnlazada.java
@@ -2,115 +2,116 @@ package dll;
 
 public class ListaDoblementeEnlazada {
 
-	private NodoDoblementeEnlazado PrimerNodo;
-	private NodoDoblementeEnlazado UltimoNodo;
-	
-	public ListaDoblementeEnlazada(){
-		PrimerNodo = null;
-		UltimoNodo = null;
-	}
-	
-	public ListaDoblementeEnlazada(NodoDoblementeEnlazado primero, NodoDoblementeEnlazado ultimo){
-		PrimerNodo = primero;
-		UltimoNodo = ultimo;
-	}
-	
-	public void InsertarAdelante(ListaDoblementeEnlazada lista, NodoDoblementeEnlazado nodo, NodoDoblementeEnlazado nuevonodo){
-		nuevonodo.SetAnterior(nodo);
-		nuevonodo.SetSiguiente(nodo.Siguiente());
-		if(nodo.Siguiente() == null){
-			lista.UltimoNodo = nuevonodo;
-		}
-		else{
-			nodo.Siguiente().SetAnterior(nuevonodo);
-			nodo.SetSiguiente(nuevonodo);
-		}
-	}
-	
-	public void InsertarAtras(ListaDoblementeEnlazada lista, NodoDoblementeEnlazado nodo, NodoDoblementeEnlazado nuevonodo){
-		nuevonodo.SetAnterior(nodo.Anterior());
-		nuevonodo.SetSiguiente(nodo);
-		if(nodo.Anterior() == null){
-			lista.PrimerNodo = nuevonodo;
-		}
-		else{
-			nodo.Anterior().SetSiguiente(nuevonodo);
-			nodo.SetAnterior(nuevonodo);
-			
-		}
-	}
-	
-	public void InsertarAlPrincipio(ListaDoblementeEnlazada lista, NodoDoblementeEnlazado nodo){
-		
-	if (lista.PrimerNodo == null){
-		lista.PrimerNodo = nodo;
-		lista.UltimoNodo = nodo;
-		nodo.SetSiguiente(null);
-		nodo.SetAnterior(null);		
-		}
-	else{
-		InsertarAtras(lista, lista.PrimerNodo, nodo);
-		}
-	}
-	
-	public void InsertarAlFinal(ListaDoblementeEnlazada lista, NodoDoblementeEnlazado nodo){
-		if(lista.UltimoNodo == null){
-			lista.InsertarAlPrincipio(lista,nodo);
-		}
-		else{
-			lista.InsertarAdelante(lista, lista.UltimoNodo, nodo);
-		}
-	}
-	
-	public void EliminarNodo(ListaDoblementeEnlazada lista, NodoDoblementeEnlazado nodo){
-	
-		if(nodo.Anterior() == null){
-			lista.PrimerNodo = nodo.Siguiente();
-		}
-		else{
-			nodo.Anterior().SetSiguiente(nodo.Siguiente());
-		}
-		if(nodo.Siguiente() == null){
-			lista.UltimoNodo = nodo.Anterior();
-		}
-		else{
-			nodo.Siguiente().SetAnterior(nodo.Anterior());
-		}
-		
-	}
-	
+    private NodoDoblementeEnlazado primerNodo;
+    private NodoDoblementeEnlazado ultimoNodo;
+
+    public ListaDoblementeEnlazada() {
+        primerNodo = null;
+        ultimoNodo = null;
+    }
+
+    public ListaDoblementeEnlazada(NodoDoblementeEnlazado primero,
+            NodoDoblementeEnlazado ultimo) {
+        primerNodo = primero;
+        ultimoNodo = ultimo;
+    }
+
+    public void insertarAdelante(ListaDoblementeEnlazada lista,
+            NodoDoblementeEnlazado nodo, NodoDoblementeEnlazado nuevonodo) {
+        nuevonodo.setAnterior(nodo);
+        nuevonodo.setSiguiente(nodo.siguiente());
+        if (nodo.siguiente() == null) {
+            lista.ultimoNodo = nuevonodo;
+        } else {
+            nodo.siguiente().setAnterior(nuevonodo);
+            nodo.setSiguiente(nuevonodo);
+        }
+    }
+
+    public void insertarAtras(ListaDoblementeEnlazada lista,
+            NodoDoblementeEnlazado nodo, NodoDoblementeEnlazado nuevonodo) {
+        nuevonodo.setAnterior(nodo.anterior());
+        nuevonodo.setSiguiente(nodo);
+        if (nodo.anterior() == null) {
+            lista.primerNodo = nuevonodo;
+        } else {
+            nodo.anterior().setSiguiente(nuevonodo);
+            nodo.setAnterior(nuevonodo);
+
+        }
+    }
+
+    public void insertarAlPrincipio(ListaDoblementeEnlazada lista,
+            NodoDoblementeEnlazado nodo) {
+
+        if (lista.primerNodo == null) {
+            lista.primerNodo = nodo;
+            lista.ultimoNodo = nodo;
+            nodo.setSiguiente(null);
+            nodo.setAnterior(null);
+         } else {
+            insertarAtras(lista, lista.primerNodo, nodo);
+        }
+    }
+
+    public void insertarAlFinal(ListaDoblementeEnlazada lista,
+        NodoDoblementeEnlazado nodo) {
+        if (lista.ultimoNodo == null) {
+            lista.insertarAlPrincipio(lista, nodo);
+        } else {
+            lista.insertarAdelante(lista, lista.ultimoNodo, nodo);
+        }
+    }
+
+    public void eliminarNodo(ListaDoblementeEnlazada lista,
+            NodoDoblementeEnlazado nodo) {
+
+        if (nodo.anterior() == null) {
+            lista.primerNodo = nodo.siguiente();
+        } else {
+            nodo.anterior().setSiguiente(nodo.siguiente());
+        }
+        if (nodo.siguiente() == null) {
+            lista.ultimoNodo = nodo.anterior();
+        } else {
+            nodo.siguiente().setAnterior(nodo.anterior());
+        }
+
+    }
+
 }
 
- class NodoDoblementeEnlazado{
-	
-	 private NodoDoblementeEnlazado anterior,siguiente;
-	 private int dato;
-	 
-	 public NodoDoblementeEnlazado(NodoDoblementeEnlazado anterior,int dato, NodoDoblementeEnlazado siguiente){
-		 
-		 this.anterior = anterior;
-		 this.dato = dato;
-		 this.siguiente = siguiente;
-		 
-	 }
-	 
-	 public NodoDoblementeEnlazado Siguiente(){
-		 return this.siguiente;
-	 }
-	 
-	 public NodoDoblementeEnlazado Anterior(){
-		 return this.anterior;
-	 }
-	 
-	 public int Dato(){
-		 return this.dato;
-	 }
-	 
-	 public void SetSiguiente(NodoDoblementeEnlazado nuevo){
-		 this.siguiente = nuevo;
-	 }
-	 
-	 public void SetAnterior(NodoDoblementeEnlazado nuevo){
-		 this.anterior = nuevo;
-	 }
+class NodoDoblementeEnlazado {
+
+    private NodoDoblementeEnlazado anterior, siguiente;
+    private int dato;
+
+    public NodoDoblementeEnlazado(NodoDoblementeEnlazado anterior, int dato,
+            NodoDoblementeEnlazado siguiente) {
+
+        this.anterior = anterior;
+        this.dato = dato;
+        this.siguiente = siguiente;
+
+    }
+
+    public NodoDoblementeEnlazado siguiente() {
+        return this.siguiente;
+    }
+
+    public NodoDoblementeEnlazado anterior() {
+        return this.anterior;
+    }
+
+    public int dato() {
+        return dato;
+    }
+
+    public void setSiguiente(NodoDoblementeEnlazado nuevo) {
+        this.siguiente = nuevo;
+    }
+
+    public void setAnterior(NodoDoblementeEnlazado nuevo) {
+        this.anterior = nuevo;
+    }
 }
